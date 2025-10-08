89200e7ab94c41aa80677b7ebac466ed32db4cc7
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-10-08 16:55:20 +0700

    Completing the PlatformsController and creating views for Platforms

--- app/Http/Controllers/PlatformsController.php
+++ app/Http/Controllers/PlatformsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Platform;
 use Illuminate\Http\Request;
 
 class PlatformsController extends Controller
@@ -11,7 +12,8 @@ class PlatformsController extends Controller
      */
     public function index()
     {
-        //
+        $platforms = Platform::latest()->paginate(10);
+        return view('platforms.index', compact('platforms'));
     }
 
     /**
@@ -19,7 +21,7 @@ class PlatformsController extends Controller
      */
     public function create()
     {
-        //
+        return view('platforms.create');
     }
 
     /**
@@ -27,8 +29,15 @@ class PlatformsController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $request->validate([
+            'name' => 'required|unique:platforms,name|max:255',
+        ]);
+
+        Platform::create($request->all());
+
+        return redirect()->route('platforms.index')
+                        ->with('success', 'Platform has been added successfully.');
+        }
 
     /**
      * Display the specified resource.
@@ -41,24 +50,35 @@ class PlatformsController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Platform $platform)
     {
-        //
+        return view('platforms.edit', compact('platform'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Platform $platform)
     {
-        //
+        $request->validate([
+            // Excludes current ID in order not to get the same name when updating
+            'name' => 'required|unique:platforms,name,'.$platform->id.'|max:255', 
+        ]);
+
+        $platform->update($request->all());
+
+        return redirect()->route('platforms.index')
+                        ->with('success', 'Platform has been updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Platform $platform)
     {
-        //
+        $platform->delete();
+
+        return redirect()->route('platforms.index')
+                        ->with('success', 'Platform has been deleted successfully.');
     }
 }

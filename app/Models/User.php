62dcfd5f406894d053b720b404641a1cc30760eb
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-11-28 22:00:50 +0700

    Implement relationship between user and order in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Order;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Get the orders associated with the user.
+     */
+    public function orders()
+    {
+        // A user can have many orders (One-to-Many relationship)
+        return $this->hasMany(Order::class);
     }
 }

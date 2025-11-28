be61c55b75dd73231b9087a21ec83ef1b35c1ac3
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-11-28 22:33:55 +0700

    Try to fix something but it is not working

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
+use App\Models\Order;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Models\Order;
 
 class User extends Authenticatable
 {

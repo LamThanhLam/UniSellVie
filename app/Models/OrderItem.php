dd80d4db491bd8c6f2ace3436bab4acb08d82347
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-11-28 21:28:07 +0700

    The same goes for OrderItem model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'price', // Essential for historical record
+        'quantity',
+    ];
+
+    // An OrderItem belongs to an Order
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // An OrderItem belongs to a Product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }

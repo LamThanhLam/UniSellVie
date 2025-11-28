386a0608601c314b766d9a60037802e189f5b6c0
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-11-29 06:47:57 +0700

    Working on the common layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,80 +2,172 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'UniSellVie') }}</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <!-- Favicon -->
+    <link href="{{ asset('template_assets/img/favicon.ico') }}" rel="icon"> 
+    
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet"> 
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('template_assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('template_assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" /> 
+    
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('template_assets/css/bootstrap.min.css') }}" rel="stylesheet"> 
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('template_assets/css/style.css') }}" rel="stylesheet"> 
 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+        <div class="container-fluid position-relative d-flex p-0">  
+            <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        
+        <!-- Sidebar area -->
+        <div class="sidebar pe-4 pb-3">
+            <nav class="navbar bg-secondary navbar-dark">
+                <a href="{{ url('/') }}" class="navbar-brand mx-4 mb-3"> <h3 class="text-primary"></i>UniSellVie</h3>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                
+                <div class="d-flex align-items-center ms-4 mb-4">
+                    <div class="ms-3">
+                        <h6 class="mb-0">{{ Auth::user()->name ?? 'Guest' }}</h6> <span>{{ Auth::check() ? 'Admin' : 'Guest' }}</span> </div>
+                </div>
+                
+                <div class="navbar-nav w-100">
+                    <a href="{{ route('home') }}" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
+                    <a href="{{ route('products.index') }}" class="nav-item nav-link">
+                        <i class="fa fa-gamepad me-2"></i>Manage Product</a> 
+                    <a href="{{ route('platforms.index') }}" class="nav-item nav-link">
+                        <i class="fa fa-desktop me-2"></i>Manage Platform</a> 
+                    <a href="{{ route('genres.index') }}" class="nav-item nav-link">
+                        <i class="fa fa-tags me-2"></i>Manage Genre</a> @auth
+                    <a href="{{ route('logout') }}" class="nav-item nav-link" 
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fa fa-sign-out-alt me-2"></i>Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                    @endauth
+                </div>
+            </nav>
+        </div>
+        <!-- End of sidebar area -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+        <!-- Content area -->
+        <div class="content">
+            <!-- Navigation bar area -->
+            <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
+                <a href="{{ url('/') }}" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <form class="d-none d-md-flex ms-4">
+                    <input class="form-control bg-dark border-0" type="search" placeholder="Search">
+                </form>
+                
+                <div class="navbar-nav align-items-center ms-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                                <span class="text-white d-none d-lg-inline-flex">{{ Auth::user()->name ?? 'Guest' }}</span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
+                                    <!-- <a href="#" class="dropdown-item">My Profile</a>
+                                    <a href="#" class="dropdown-item">Settings</a> -->
+                                    <a href="{{ route('logout') }}" class="dropdown-item"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                            </div>
+                        </div>
+                    @endguest
+                </div>
+            </nav>
+            <!-- End of navigation bar area -->
+
+                <!-- <main class="py-4">
+                    @yield('content')
+                </main> -->
+            
+            <!-- Footer area -->
+            <div class="container-fluid pt-4 px-4">
+                @yield('content') </div>
+            <div class="container-fluid pt-4 px-4">
+                <div class="bg-secondary rounded-top p-4">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 text-center text-sm-start">
+                            &copy; <a href="#">UniSellVie</a>, All Right Reserved. 
+                        </div>
+                        <div class="col-12 col-sm-6 text-center text-sm-end">
+                            Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                            <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
+            <!-- End of footer area -->
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        </div>
+        <!-- End of content area -->
+
+        <!-- Back to top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('template_assets/lib/chart/chart.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/easing/easing.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/waypoints/waypoints.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/owlcarousel/owl.carousel.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/tempusdominus/js/moment.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/tempusdominus/js/moment-timezone.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script> 
+    
+    <!-- Template Javascript -->
+    <script src="{{ asset('template_assets/js/main.js') }}"></script> 
+
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,28 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}"> <title>UniSellVie</title>
     
-    <link href="{{ asset('template_assets/img/favicon.ico') }}" rel="icon"> <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- Favicon -->
+    <link href="{{ asset('template_assets/img/favicon.ico') }}" rel="icon"> 
+    
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@500;700&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    <link href="{{ asset('template_assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet"> <link href="{{ asset('template_assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" /> <link href="{{ asset('template_assets/css/bootstrap.min.css') }}" rel="stylesheet"> <link href="{{ asset('template_assets/css/style.css') }}" rel="stylesheet"> </head>
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('template_assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('template_assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" /> 
+    
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('template_assets/css/bootstrap.min.css') }}" rel="stylesheet"> 
+    
+    <!-- Template Stylesheet -->
+    <link href="{{ asset('template_assets/css/style.css') }}" rel="stylesheet"> 
+</head>
 
 <body>
     <div class="container-fluid position-relative d-flex p-0">
@@ -21,6 +36,8 @@
                 <span class="sr-only">Loading...</span>
             </div>
         </div>
+        
+        <!-- Sidebar area -->
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-secondary navbar-dark">
                 <a href="{{ url('/') }}" class="navbar-brand mx-4 mb-3"> <h3 class="text-primary"></i>UniSellVie</h3>
@@ -33,7 +50,12 @@
                 
                 <div class="navbar-nav w-100">
                     <a href="{{ route('home') }}" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-                    <a href="{{ route('products.index') }}" class="nav-item nav-link"><i class="fa fa-gamepad me-2"></i>Manage Product</a> <a href="{{ route('platforms.index') }}" class="nav-item nav-link"><i class="fa fa-desktop me-2"></i>Quản lý Nền tảng</a> <a href="{{ route('genres.index') }}" class="nav-item nav-link"><i class="fa fa-tags me-2"></i>Quản lý Thể loại</a> @auth
+                    <a href="{{ route('products.index') }}" class="nav-item nav-link">
+                        <i class="fa fa-gamepad me-2"></i>Manage Product</a> 
+                    <a href="{{ route('platforms.index') }}" class="nav-item nav-link">
+                        <i class="fa fa-desktop me-2"></i>Manage Platform</a> 
+                    <a href="{{ route('genres.index') }}" class="nav-item nav-link">
+                        <i class="fa fa-tags me-2"></i>Manage Genre</a> @auth
                     <a href="{{ route('logout') }}" class="nav-item nav-link" 
                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                         <i class="fa fa-sign-out-alt me-2"></i>Logout
@@ -45,7 +67,11 @@
                 </div>
             </nav>
         </div>
+        <!-- End of sidebar area -->
+
+        <!-- Content area -->
         <div class="content">
+            <!-- Navigation bar area -->
             <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
                 <a href="{{ url('/') }}" class="navbar-brand d-flex d-lg-none me-4">
                     <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
@@ -58,26 +84,44 @@
                 </form>
                 
                 <div class="navbar-nav align-items-center ms-auto">
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <span class="text-white d-none d-lg-inline-flex">{{ Auth::user()->name ?? 'Guest' }}</span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-                                <!-- <a href="#" class="dropdown-item">My Profile</a>
-                                <a href="#" class="dropdown-item">Settings</a> -->
-                                <a href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                                <span class="text-white d-none d-lg-inline-flex">{{ Auth::user()->name ?? 'Guest' }}</span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
+                                    <!-- <a href="#" class="dropdown-item">My Profile</a>
+                                    <a href="#" class="dropdown-item">Settings</a> -->
+                                    <a href="{{ route('logout') }}" class="dropdown-item"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                            </div>
                         </div>
-                    </div>
+                    @endguest
                 </div>
             </nav>
+            <!-- End of navigation bar area -->
+
+            <!-- Footer area -->
             <div class="container-fluid pt-4 px-4">
                 @yield('content') </div>
             <div class="container-fluid pt-4 px-4">
@@ -93,12 +137,30 @@
                     </div>
                 </div>
             </div>
-            </div>
-        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-    </div>
+            <!-- End of footer area -->
 
+        </div>
+        <!-- End of content area -->
+
+        <!-- Back to top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+    </div>
+    
+    <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('template_assets/lib/chart/chart.min.js') }}"></script> <script src="{{ asset('template_assets/lib/easing/easing.min.js') }}"></script> <script src="{{ asset('template_assets/lib/waypoints/waypoints.min.js') }}"></script> <script src="{{ asset('template_assets/lib/owlcarousel/owl.carousel.min.js') }}"></script> <script src="{{ asset('template_assets/lib/tempusdominus/js/moment.min.js') }}"></script> <script src="{{ asset('template_assets/lib/tempusdominus/js/moment-timezone.min.js') }}"></script> <script src="{{ asset('template_assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script> <script src="{{ asset('template_assets/js/main.js') }}"></script> </body>
+    <script src="{{ asset('template_assets/lib/chart/chart.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/easing/easing.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/waypoints/waypoints.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/owlcarousel/owl.carousel.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/tempusdominus/js/moment.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/tempusdominus/js/moment-timezone.min.js') }}"></script> 
+    <script src="{{ asset('template_assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script> 
+    
+    <!-- Template Javascript -->
+    <script src="{{ asset('template_assets/js/main.js') }}"></script> 
+
+</body>
 
 </html>

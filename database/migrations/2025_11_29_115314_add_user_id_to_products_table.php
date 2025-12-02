2091512c021df8a7140d5d3f0c4ea653af5b534a
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-12-02 17:52:42 +0700

    Have to fix everything

--- database/migrations/2025_11_29_115314_add_user_id_to_products_table.php
+++ database/migrations/2025_11_29_115314_add_user_id_to_products_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -11,6 +12,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::table('products', function (Blueprint $table) {
+            // Temporary add column as nullable
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+        });
+        
+        // Add foreign key (after data is cleared)
         Schema::table('products', function (Blueprint $table) {
             // Add foreign key reference to id of users table, and automatically delete product when user get deleted
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();

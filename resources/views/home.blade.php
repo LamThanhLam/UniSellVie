60944657d918836a099851395b5d58ded95a2d14
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-12-05 00:26:28 +0700

    Completing the Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,6 @@
                 @endif
                 <form>
                     <div class="actions">
-                        <a href="{{ route('products.create') }}" class="btn btn-primary">Add New Product</a>
                         <form action="{{ route('products.index') }}" method="GET" class="d-none d-md-flex ms-4 col-sm-4 col-form-label">
                             <input class="form-control bg-dark border-0" type="text" name="search" placeholder="Search for product..." value="{{ request('search') }}">
                             <button type="submit">Search</button>
@@ -27,37 +26,27 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th>Ảnh</th> <th>Tiêu đề</th>
-                    <th>Ngày phát hành</th>
-                    <th>Nhà phát triển</th>
-                    <th>Nhà xuất bản</th>
-                    <th width="280px">Hành động</th>
+                    <th>Title</th>
+                    <th>Releashed date</th>
+                    <th width="280px">Action</th>
                 </tr>
             </thead>
             <tbody>
                 @forelse ($products as $product)
-                    <tr>
-                    <td> @if ($product->image)
-                            <img src="{{ asset('images/' . $product->image) }}" width="50px" height="50px" alt="Product image">
-                        @else
-                            <img src="{{ asset('images/default.png') }}" width="50px" height="50px" alt="No image">
-                        @endif
-                    </td>    
+                    <tr>  
                     <td><a href="{{ route('products.show', $product->id) }}">{{ $product->title }}</a></td>
                         <td>{{ $product->releaseDate->format('d/m/Y') }}</td>
-                        <td>{{ $product->developer }}</td>
-                        <td>{{ $product->publisher }}</td>
                         <td>
-                            <a href="{{ route('products.show', $product->id) }}">{{ $product->title }}</a>
+                            <a href="{{ route('products.show', $product->id) }}" class="btn btn-primary"><i class="fa fa-eye me"></i></a>
                             @can('update', $product)
-                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary">Sửa</a>
+                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary"><i class="fa fa-pen me"></i></a>
                             @endcan
 
                             @can('delete', $product)
                                 <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline-block;">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Bạn có chắc chắn muốn xóa sản phẩm này không?');">Xóa</button>
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this game?');"><i class="fa fa-trash me"></i></button>
                                 </form>
                             @endcan
                         </td>

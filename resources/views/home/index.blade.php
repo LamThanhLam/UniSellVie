9e50574bf30039e55b93160b3132248335b2699d
LamThanhLam <lamltgcc210360@fpt.edu.vn>
2025-12-05 17:11:04 +0700

    Oops

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,65 +3,68 @@
 @section('content')
 <div class="container-fluid pt-4 px-4">
     <div class="row g-4">
-        <div class="col-sm-12 col-xl-6">
+        <div class="col-sm-12 col-xl-12">
             <div class="bg-secondary rounded h-100 p-4">
-                <h1>List Of Products</h1>
+                <h1>Welcome to UniSellVie Store</h1>
                 
                 @if(session('success'))
-                    <div class="alert">
+                    <div class="alert alert-success">
                         {{ session('success') }}
                     </div>
                 @endif
-                <form>
-                    <div class="actions">
-                        <form action="{{ route('products.index') }}" method="GET" class="d-none d-md-flex ms-4 col-sm-4 col-form-label">
-                            <input class="form-control bg-dark border-0" type="text" name="search" placeholder="Search for product..." value="{{ request('search') }}">
-                            <button type="submit">Search</button>
-                        </form>
-                    </div>
+                
+                <!-- Search Form -->
+                <form action="{{ route('home.index') }}" method="GET" class="d-flex align-items-center mb-4">
+                    <input class="form-control bg-dark border-0 me-2" type="text" name="search" placeholder="Search for product..." value="{{ request('search') }}">
+                    <button type="submit" class="btn btn-primary">Search</button>
                 </form>
             </div>
         </div>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Releashed date</th>
-                    <th width="280px">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($products as $product)
-                    <tr>  
-                    <td><a href="{{ route('home.show', $product->id) }}">{{ $product->title }}</a></td>
-                        <td>{{ $product->releaseDate->format('d/m/Y') }}</td>
-                        <td>
-                            <a href="{{ route('home.show', $product->id) }}" class="btn btn-primary"><i class="fa fa-eye me"></i></a>
-                            @can('update', $product)
-                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary"><i class="fa fa-pen me"></i></a>
-                            @endcan
-
-                            @can('delete', $product)
-                                <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline-block;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this game?');"><i class="fa fa-trash me"></i></button>
-                                </form>
-                            @endcan
-                        </td>
-                    </tr>
-                @empty
+    </div>
+    
+    <!-- Display products as table -->
+    <div class="row mt-3">
+        <div class="col-12">
+            <table class="table table-bordered table-dark">
+                <thead>
                     <tr>
-                        <td colspan="5">There is no product.</td>
+                        <th width="40%">Title</th>
+                        <th width="15%">Released date</th>
+                        <th width="20%">Developer</th>
+                        <th width="15%">Price</th>
+                        <th width="10%">Action</th>
                     </tr>
-                @endforelse
-            </tbody>
-        </table>
-
-        <div class="pagination">
-            {{ $products->links() }}
+                </thead>
+                <tbody>
+                    @forelse ($products as $product)
+                        <tr> 
+                            <td>
+                                <!-- link to published detail page -->
+                                <a href="{{ route('home.show', $product->id) }}">{{ $product->title }}</a>
+                            </td>
+                            <td>{{ $product->releaseDate->format('m/d/Y') }}</td>
+                            <td>{{ $product->developer }}</td>
+                            <td>{{ number_format($product->price) }} VNĐ</td>
+                            <td>
+                                <!-- View button (detail) and purchase button (simulated) -->
+                                <a href="{{ route('home.show', $product->id) }}" class="btn btn-info btn-sm">
+                                    <i class="fa fa-eye me-0"></i> View
+                                </a>
+                                {{-- The purchase button will be displayed in show page --}}
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5">There is no product available in the store.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
+    </div>
+
+    <div class="d-flex justify-content-center">
+        {{ $products->links() }}
     </div>
 </div>
 @endsection
